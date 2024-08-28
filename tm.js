959318c63acd3f4ebdd61577488ad4e04737a27b
theogbob <168785618+theogbob@users.noreply.github.com>
2024-08-28 18:04:39 -0400

    Create tm.js

---
+++ tm.js
@@ -0,0 +1,123 @@
+// ==UserScript==
+// @name         Auto Solver with Materialize
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Script to auto-solve problems, navigate, and load Materialize CSS/JS
+// @author       Your Name
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Load Materialize CSS
+    var materializeCSS = document.createElement('link');
+    materializeCSS.rel = 'stylesheet';
+    materializeCSS.href = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css';
+    document.head.appendChild(materializeCSS);
+
+    // Load Materialize JS
+    var materializeJS = document.createElement('script');
+    materializeJS.src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js';
+    document.head.appendChild(materializeJS);
+
+    // Wait until Materialize JS is fully loaded
+    function onMaterializeReady(callback) {
+        const checkInterval = setInterval(function() {
+            if (typeof M !== 'undefined') {
+                clearInterval(checkInterval);
+                callback();
+            }
+        }, 100);
+    }
+
+    // Main script functionality
+    onMaterializeReady(function() {
+        // Array of strings used in the script
+        var strings = [
+            '#dashboardButton', 
+            "Made by FOXYFIASCO aka HAMZINIII! If konrad says its him, tell him he has molasses in his head. :)", 
+            'querySelector', 
+            'body', 
+            'addEventListener', 
+            'keypress', 
+            'qwer', 
+            'split', 
+            'key', 
+            "Directions: click q to solve, w to go to the next question, and e to return to dashboard when you are at the last question", 
+            'toArray', 
+            'toast', 
+            '#checkAnswersButton', 
+            'click', 
+            "Next Problem", 
+            '#nextProblemButton', 
+            "Returning to dashboard"
+        ];
+
+        // Function to access the strings array
+        var getString = function(index) {
+            return strings[index];
+        };
+
+        // Display initial message
+        M.toast({
+            'html': getString(1),
+            'displayLength': 4000
+        });
+
+        // Function to solve and check answers
+        const solveAndCheckAnswers = async () => {
+            $("[name='answer']").toArray().forEach((element, index) => {
+                element.value = answerValues[index];
+            });
+            M.toast({
+                'html': 'Solved',
+                'displayLength': 1000
+            });
+            $(getString(12)).trigger(getString(13));
+        };
+
+        // Function to go to the next problem
+        const goToNextProblem = () => {
+            M.toast({
+                'html': getString(14),
+                'displayLength': 1000
+            });
+            $(getString(15)).trigger(getString(13));
+        };
+
+        // Function to return to dashboard
+        const returnToDashboard = () => {
+            M.toast({
+                'html': getString(16),
+                'displayLength': 1000
+            });
+            $(getString(0)).trigger(getString(13));
+        };
+
+        // Function to display directions
+        const showDirections = () => {
+            alert(getString(9));
+        };
+
+        // Add keypress event listener to the body
+        document.querySelector(getString(3)).addEventListener(getString(5), event => {
+            let keys = getString(6).split('');
+            switch (event.key) {
+                case keys[0]:
+                    solveAndCheckAnswers();
+                    break;
+                case keys[1]:
+                    goToNextProblem();
+                    break;
+                case keys[2]:
+                    returnToDashboard();
+                    break;
+                case keys[3]:
+                    showDirections();
+                    break;
+            }
+        });
+    });
+})();
